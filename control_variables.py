0bdcb711c0a5c99fa11145736b6397aefd4fce66
Ester <chowed85@gmail.com>
2021-09-24 12:57:46 -0400

    moved script files into subfolder

--- control_variables.py
+++
@@ -1,4 +0,0 @@
-import os
-hyperion_path = os.path.join(os.path.dirname(os.getcwd()),"forge-dataviz-iot-reference-app\server\gateways\csv\\")
-json_path = os.path.join(os.path.dirname(os.getcwd()),"forge-dataviz-iot-reference-app\server\gateways\synthetic-data")
-reference_file_path = os.path.join(os.getcwd(),"reference_data")
